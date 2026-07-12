5e45a4e5f30477d729d90f2bc6e1348ca897800a
macha0418hiro-creator <macha0418hiro@gmail.com>
2026-07-12 16:55:11 +0900

    BossAttackSpriteAdd
    ボスの攻撃に画像を当てた。
    ボスによって画像を切り替える処理を追加。
    ブランの専用技は処理を変更する必要あり。

--- Assets/Script/BossScript/Wizard/HomingBullet.cs
+++ Assets/Script/BossScript/Wizard/HomingBullet.cs
@@ -2,6 +2,16 @@ using UnityEngine;
 
 public class HomingBullet : MonoBehaviour
 {
+    [System.Serializable]
+    public struct VisualSetup
+    {
+        public Sprite bulletSprite; // 弾の画像
+    }
+
+    [Header("ボスの色に合わせた見た目の設定")]
+    [SerializeField] private VisualSetup setupForWhiteBoss; //白用のSprite
+    [SerializeField] private VisualSetup setupForBlackBoss; //黒用のSprite
+
     [Header("移動速度")]
     [SerializeField] private float speed = 5f;
 
@@ -14,6 +24,19 @@ public class HomingBullet : MonoBehaviour
     private Transform playerTransform;
     private Rigidbody2D rb;
     private float timer = 0f;
+    private SpriteRenderer spriteRenderer;
+
+    //弾の見た目をボスに合わせて適応
+    public void SetUpBullet(bool isWhiteBoss)
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        VisualSetup currentSetup = isWhiteBoss ? setupForWhiteBoss : setupForBlackBoss;
+
+        if(spriteRenderer != null && currentSetup.bulletSprite != null)
+        {
+            spriteRenderer.sprite = currentSetup.bulletSprite;
+        }
+    }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
